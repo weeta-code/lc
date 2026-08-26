d8fda34d21d23061770c2b5f7fcc3332b961b41c
weeta-code <victordesouza132@gmail.com>
2026-08-26 18:12:03 -0400

    14

---
+++ 14.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for (int i {}; i < nums.size(); i++) {
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i] == nums[i-1] ) continue;
+
+            int l = i + 1;
+            int r = nums.size() -1;
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
